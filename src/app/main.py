dae531fc6ad8a5fbac88c9fe7f40b90a3c181a47
Conty111 <mitj88@gmail.com>
2024-05-23 13:58:08 +0300

    simple example by notes

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,15 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.api import ping
+from app.api import notes, ping
+from app.db.db import engine, metadata, database
 
 app = FastAPI()
 
+@asynccontextmanager
+async def lifespan():
+    await database.connect()
+    yield
+    await database.disconnect()
+
 app.include_router(ping.router)
+app.include_router(notes.router, prefix="/notes", tags=["notes"])
---
+++ src/app/services/crud.py
@@ -0,0 +1,10 @@
+from app.db.models import NoteSchema
+from app.db.db import notes, database
+
+async def post(payload: NoteSchema):
+    query = notes.insert().values(title=payload.title, description=payload.description)
+    return await database.execute(query=query)
+
+async def get(id: int):
+    query = notes.select().where(id == notes.c.id)
+    return await database.fetch_one(query=query)
